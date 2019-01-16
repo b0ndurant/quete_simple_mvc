d6ea9454060692ee17a632b8b57a99fc45cd6c53
b0ndurant <martin.aurelien01@gmail.com>
2019-01-16 16:35:50 +0100

    add composer twig,add twig in all controller and add view twig

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -2,21 +2,35 @@
 
 namespace Wcs\Controller;
 use Wcs\Model\ItemManager;
+use Twig_Loader_Filesystem;
+use Twig_Environment;
 // src/Controller/ItemController.php
 
 class ItemController {
+
+	private $twig;
+
+    public function __construct() 
+    {
+        $loader = new Twig_Loader_Filesystem(__DIR__.'/../View');
+        $this->twig = new Twig_Environment($loader);
+    }
 
 	public function index() {
 		$itemManager = new ItemManager();
 		$items = $itemManager->selectAllItems();
 
-		require __DIR__ . '/../View/item.php';
+		return $this->twig->render('Item/index.html.twig', [
+			'items' => $items
+		]);
 	}
 
 	public function show(int $id) {
 		$itemManager = new ItemManager();
 		$item = $itemManager->selectOneItem($id);
 
-		require __DIR__ . '/../View/showItem.php';
+		return $this->twig->render('Item/showItem.html.twig', [
+			'item' => $item
+		]);
 	} 
 }
